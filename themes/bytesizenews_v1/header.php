c40ad73da51e1bf115c1d20e4d4ceee0030d4e86
Tim Smith <tim@timothybsmith.com>
2013-07-20 16:43:00 -0500

    Adding signature to header

--- themes/bytesizenews_v1/header.php
+++ themes/bytesizenews_v1/header.php
@@ -2,13 +2,32 @@
 <html lang="en">
 
 <head>
+  
+  <!-- 
+
+  @@@@@@@@@@@@@@@@@  @@@   @@@          @@@ 
+  @@@@@@@@@@@@@@@@@  @@@   @@@@        @@@@
+         @@@         @@@   @@@ @      @ @@@
+         @@@         @@@   @@@  @@  @@  @@@
+         @@@         @@@   @@@   @@@    @@@
+         @@@         @@@   @@@          @@@
+         @@@         @@@   @@@          @@@
+         @@@         @@@   @@@          @@@
+
+  How much does a polar bear weigh? Just enough to break the ice.
+
+  Made with love in beautiful Saint Paul, Minnesota.
+  The East Wing © 2013 Anythin’ Goes LLC and Timothy B. Smith.
+
+  -->
+
   <meta charset="utf-8" />
   <title><?php is_home() ? bloginfo('description') : wp_title(''); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
   
   <!-- My styles -->
-  <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/css/global.css"/>
+  <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/assets/css/global.css"/>
 
   <!-- Typekit Code -->
   <script type="text/javascript" src="//use.typekit.net/vnu4dui.js"></script>
@@ -20,14 +39,14 @@
   <!--End WP Generated Header -->
   
 </head>
-<body>
+<body <?php body_class($class); ?>>
   
 
   <header class="site__header" role="banner"> 
     <div class="contain"> 
       <nav class="site__nav" role="navigation">
         <ul>
-          <li><a href="#">Episodes</a></li>
+          <li><a href="/">Episodes</a></li>
           <li><a href="#">Sponsorship</a></li>
           <li><a href="#">Contact</a></li>
         </ul>
