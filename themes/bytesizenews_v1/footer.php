3d3745ebe0650b118dedf30d0bcb6114187ca836
Tim Smith <tim@timothybsmith.com>
2013-07-20 16:43:12 -0500

    Code for footer

--- themes/bytesizenews_v1/footer.php
+++ themes/bytesizenews_v1/footer.php
@@ -1,3 +1,8 @@
+<footer class="site__footer" role="contentinfo">
+	<div class="contain">
+		<small><em>Bytesize</em> &copy; <?php echo date('Y'); ?> <a href="http://anythingo.es">Anythin&rsquo; Goes, LLC.</a> Site hosting by <a href="http://www.mediatemple.net/#a_aid=4f42c6d7d53a5">Media Temple</a>.</small>
+	</div>
+</footer>
 
 
 <!--WP Generated Footer -->
