b70d287f3a9d5bc31b9bc6bf7f0a88810eca74e6
Tim Smith <tim@timothybsmith.com>
2013-07-20 16:43:54 -0500

    Adding code for single page.

--- themes/bytesizenews_v1/single.php
+++ themes/bytesizenews_v1/single.php
@@ -1,4 +1,27 @@
 <?php get_header(); ?>
 
+<main class="main-content" role="main">
+	<div class="contain">
+		
+		<?php get_sidebar('single'); ?>
+
+		<section class="content-column">
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				
+				<article class="entry">
+					<h2><a href="<?php the_permalink(); ?>" title="Link to <?php the_title(); ?>">#<?php the_field('episode_number');?>: <?php the_title(); ?></a></h2>
+					<p class="pubdate"><time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('F jS, Y');?></time> &middot; <?php the_field('episode_length'); ?> minutes</p>
+					<?php the_content(); ?>
+				</article>
+
+			<?php endwhile; endif; ?>
+
+			<div class="page__navigation">
+				<?php wp_pagenavi(); ?>
+			</div>
+		</section><!-- .content-column -->
+	</div><!-- .contain -->
+</main>
+
 
 <?php get_footer(); ?>
