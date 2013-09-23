5f07ee3ad6a312d99afaa40448c39fe9f652a53f
Tim Smith <smith@ttimsmith.com>
2013-09-23 14:56:47 -0500

    Changing sponsor area to say Bytesize

--- themes/bytesizenews_v1/single.php
+++ themes/bytesizenews_v1/single.php
@@ -2,26 +2,26 @@
 
 <main class="main-content" role="main">
 	<div class="contain">
-		
+
 		<?php get_sidebar('single'); ?>
 
 		<section class="content-column">
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-				
+
 				<article class="entry">
 					<h2>#<?php the_field('episode_number');?>: <?php the_title(); ?></h2>
 					<p class="pubdate"><time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('F jS, Y');?></time> &middot; <?php the_field('episode_length'); ?> minutes</p>
-					
+
 					<p><?php the_field('episode_lead'); ?></p>
-					
+
 					<?php if(get_field('episode_sponsor')) : ?>
 						<div class="entry__sponsor">
-							<p><strong>This week’s Babblenut is brought to you by:</strong></p>
+							<p><strong>This week’s Bytesize is brought to you by:</strong></p>
 							<p><?php the_field('episode_sponsor'); ?></p>
 						</div>
 						<!-- <hr> -->
-					<?php endif; ?>				
-	
+					<?php endif; ?>
+
 					<?php the_content(); ?>
 				</article>
 
